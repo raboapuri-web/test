3f62a2c665a7ed5570f81e1dcec248c89bc7acf3
raboapuri-web <raboapuri@gmail.com>
2026-09-15 00:06:06 +0900

    Remove sample animation from Remotion root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {Composition} from 'remotion';
-import {ThinkingNightSample} from './ThinkingNightSample';
+import {AbsoluteFill, Composition} from 'remotion';
+
+const Sandbox: React.FC = () => <AbsoluteFill style={{backgroundColor: '#000'}} />;
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
-      id="ThinkingNightSample"
-      component={ThinkingNightSample}
-      durationInFrames={600}
+      id="Sandbox"
+      component={Sandbox}
+      durationInFrames={30}
       fps={30}
       width={1920}
       height={1080}
