afac5aca7de1ea5b8b2d64c41aa06fc8a1e3ea15
raboapuri-web <raboapuri@gmail.com>
2026-09-15 22:30:33 +0900

    Export character templates

---
+++ src/characters/index.ts
@@ -0,0 +1,12 @@
+export {CharacterBase} from "./CharacterBase";
+export type {
+  CharacterBaseProps,
+  CharacterPose,
+  CharacterDirection,
+  CharacterOutfit,
+  CharacterProp,
+} from "./CharacterBase";
+export {MaleCharacter} from "./MaleCharacter";
+export type {MaleCharacterProps} from "./MaleCharacter";
+export {FemaleCharacter} from "./FemaleCharacter";
+export type {FemaleCharacterProps} from "./FemaleCharacter";
