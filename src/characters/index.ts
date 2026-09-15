de09624b23e812300218fdfa9406f95fc20769ad
raboapuri-web <raboapuri@gmail.com>
2026-09-16 02:35:07 +0900

    Export reusable child character components

--- src/characters/index.ts
+++ src/characters/index.ts
@@ -11,3 +11,16 @@ export {MaleCharacter} from "./MaleCharacter";
 export type {MaleCharacterProps} from "./MaleCharacter";
 export {FemaleCharacter} from "./FemaleCharacter";
 export type {FemaleCharacterProps} from "./FemaleCharacter";
+export {ChildCharacterBase} from "./ChildCharacterBase";
+export type {
+  ChildCharacterProps,
+  ChildGender,
+  ChildAge,
+  ChildPose,
+  ChildDirection,
+  ChildColors,
+} from "./ChildCharacterBase";
+export {BoyChildCharacter} from "./BoyChildCharacter";
+export type {BoyChildCharacterProps} from "./BoyChildCharacter";
+export {GirlChildCharacter} from "./GirlChildCharacter";
+export type {GirlChildCharacterProps} from "./GirlChildCharacter";
